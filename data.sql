a59cc94d325767be2d6c6a4da77da6fd2d2f6611
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2022-10-19 23:15:05 +0430

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,16 @@
+vet_clinic=#  INSERT INTO animals (ID,Name,Date_of_birth,Weight_kg,Neutered,Escape_attempts)
+vet_clinic-# VALUES (1, 'Agumon', 'Feb/3/2020', 10.23, True, 0);
+INSERT 0 1
+
+vet_clinic=#  INSERT INTO animals (ID,Name,Date_of_birth,Weight_kg,Neutered,Escape_attempts)
+vet_clinic-# VALUES (2, 'Gabumon', 'Nov/15/2018', 8, True, 2);
+INSERT 0 1
+
+vet_clinic=#  INSERT INTO animals (ID,Name,Date_of_birth,Weight_kg,Neutered,Escape_attempts)
+vet_clinic-# VALUES (3, 'Pickachu', 'Jan/7/2021', 15.04, False, 1);
+INSERT 0 1
+
+vet_clinic=# INSERT INTO animals (ID,Name,Date_of_birth,Weight_kg,Neutered,Escape_attempts)
+vet_clinic-# VALUES (4, 'Devimon', 'May/12/2017', 11, True, 5);
+INSERT 0 1
+vet_clinic=# 
