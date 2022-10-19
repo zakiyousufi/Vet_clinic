ba6f654c41896071792d0431a4c32cbf29fc5b13
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2022-10-20 01:29:40 +0430

    Update data.sql

--- data.sql
+++ data.sql
@@ -14,3 +14,31 @@ vet_clinic=# INSERT INTO animals (ID,Name,Date_of_birth,Weight_kg,Neutered,Escap
 vet_clinic-# VALUES (4, 'Devimon', 'May/12/2017', 11, True, 5);
 INSERT 0 1
 vet_clinic=# 
+
+vet_clinic=# INSERT INTO animals (ID,Name,Date_of_birth,Weight_kg,Neutered,Escape_attempts)
+VALUES (5, 'Chaemander', 'Feb/8/2020', -11, False, 0);
+INSERT 0 1
+
+vet_clinic=# INSERT INTO animals (ID,Name,Date_of_birth,Weight_kg,Neutered,Escape_attempts)
+VALUES (6, 'Plantmon', 'Nov/15/2021', -5, True, 2);
+INSERT 0 1
+
+vet_clinic=# INSERT INTO animals (ID,Name,Date_of_birth,Weight_kg,Neutered,Escape_attempts)
+VALUES (7, 'Squirtle', 'Apr/2/1993', -12.13, False, 3);
+INSERT 0 1
+
+vet_clinic=# INSERT INTO animals (ID,Name,Date_of_birth,Weight_kg,Neutered,Escape_attempts)
+VALUES (8, 'Angemon', 'Jun/12/2005', -45, True, 1);
+INSERT 0 1
+
+vet_clinic=# INSERT INTO animals (ID,Name,Date_of_birth,Weight_kg,Neutered,Escape_attempts)
+VALUES (9, 'Boarmon', 'Jun/7/2005', 20, True, 7);
+INSERT 0 1
+
+vet_clinic=# INSERT INTO animals (ID,Name,Date_of_birth,Weight_kg,Neutered,Escape_attempts)
+VALUES (10, 'Blossom', 'Oct/13/1998', 17, True, 3);
+INSERT 0 1
+
+vet_clinic=# INSERT INTO animals (ID,Name,Date_of_birth,Weight_kg,Neutered,Escape_attempts)
+VALUES (11, 'Ditto', 'May/14/2022', 22, True, 4);
+INSERT 0 1
